bd08b927255e224859e87d61ef5e618e6f38e10b
eunicehassan3 <euniceh.info@gmail.com>
2025-01-29 15:40:56 -0600

    added real-time timer to the Timer Panel

---
+++ Assets/Scripts/TimerBehavior.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking.PlayerConnection;
+
+public class TimerBehavior : MonoBehaviour
+{
+    private TextMeshProUGUI textField; 
+    private float timer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       textField =  GetComponent<TextMeshProUGUI>();
+
+       if(textField == null){
+        Debug.Log("No Components Found");
+       }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = Time.time;
+
+        int minutes = (int) timer / 60;
+        int second = (int) timer % 60;
+
+        // string message = "Time: "+ minutes + ":" + second;
+
+        string message = string.Format("<color=black>Timer: {0:00}:{1:00}", minutes, second);
+
+        textField.text = message;
+
+        // Debug.Log(timer);
+    }
+}
